27b1704888f2005fb6f229140b4fd2adb4bf3714
wehttqm <matthewfalcone5@gmail.com>
2023-08-30 12:37:49 -0400

    added dynamic routing for campaigns

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,7 +3,7 @@ import Layout from '../../components/layout';
 import { Form, Button, Input, Message } from 'semantic-ui-react';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
-import { Link, Router } from '../../routes';
+import { Router } from '../../routes';
 
 class CampaignNew extends Component {
     state = {
@@ -20,6 +20,9 @@ class CampaignNew extends Component {
             await factory.methods
             .createCampaign(this.state.minimumContribution, accounts[0])
             .send({from: accounts[0]});
+        
+        Router.pushRoute('/');
+        
         } catch (err) {
             this.setState({ errorMessage: err.message});
         }
