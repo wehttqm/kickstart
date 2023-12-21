cb6948a67b465e9e2bd8836589e0edc93b884004
wehttqm <matthewfalcone5@gmail.com>
2023-12-21 15:05:45 -0500

    added try/catch

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -6,17 +6,21 @@ const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
     process.env.MNEMONIC,
-    'https://sepolia.infura.io/v3/4a4954ee87a64cb8a617c0fb4122712c'
+    'https://sepolia.infura.io/v3/63c9b0b9b19b4f2bb4d8b0c5bbf1e7db'
 )
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
-    console.log('Attempting to deploy from account', accounts[0]);
-    const result = await new web3.eth.Contract(compiledFactory.abi)
-    .deploy({ data: compiledFactory.evm.bytecode.object })
-    .send({ gas:'1400000', from: accounts[0] })
-    console.log('Contract deployed to', result.options.address);
+    try {
+        const accounts = await web3.eth.getAccounts();
+        console.log('Attempting to deploy from account', accounts[1]);
+        const result = await new web3.eth.Contract(compiledFactory.abi)
+        .deploy({ data: compiledFactory.evm.bytecode.object })
+        .send({ gas:'1400000', from: accounts[1] })
+        console.log('Contract deployed to', result.options.address);
+    } catch (e) {
+        console.log(e)
+    }
 };
 
 deploy();
