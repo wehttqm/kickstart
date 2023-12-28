15614e0b14e1b948d8cf7753e5b513174eaae0a7
wehttqm <matthewfalcone5@gmail.com>
2023-12-27 19:49:42 -0500

    revamped deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,25 +1,30 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
 require('dotenv').config({path: '../.env'});
 const compiledFactory = require('./build/CampaignFactory.json');
- 
-
-const provider = new HDWalletProvider(
-    process.env.MNEMONIC,
-    'https://sepolia.infura.io/v3/63c9b0b9b19b4f2bb4d8b0c5bbf1e7db'
-)
+const provider = new Web3.providers.HttpProvider(process.env.SEPOLIA_RPC)
 const web3 = new Web3(provider);
 
 const deploy = async () => {
     try {
-        const accounts = await web3.eth.getAccounts();
-        console.log('Attempting to deploy from account', accounts[1]);
-        const result = await new web3.eth.Contract(compiledFactory.abi)
-        .deploy({ data: compiledFactory.evm.bytecode.object })
-        .send({ gas:'1400000', from: accounts[1] })
-        console.log('Contract deployed to', result.options.address);
+        const signer = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE);
+        console.log('Attempting to deploy from account', signer.address);
+
+        web3.eth.accounts.wallet.add(signer)
+        const contract = new web3.eth.Contract(compiledFactory.abi);
+        contract.options.data = compiledFactory.evm.bytecode.object;
+        const deployTx = contract.deploy();
+        const deployedContract = await deployTx
+        .send({ from: signer.address, gas: await deployTx.estimateGas() })
+        .on('error', (err) => {
+            console.log(err)
+        })
+        .on('transactionHash', (tx) => {
+            console.log(`Transaction: https://sepolia.etherscan.io/tx/${tx}`)
+        })
+
+        console.log('Contract deployed to', deployedContract.options.address);
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
 };
 
